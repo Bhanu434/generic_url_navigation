2ffab19e6331ba26716007e69ad6de031d2768e4
Bhanu <sandrabhanuprakash@gmail.com>
2023-11-20 22:30:15 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bhanu(request):
+    return render(request,'bhanu.html')
+def krishna(request):
+    return render(request,'krishna.html')
+def nani(request):
+    return render(request,'nani.html')
